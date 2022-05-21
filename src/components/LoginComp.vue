8d5d60e034c7e53e7ce8bae35504842c0fcf5eb4
rainbow <rainbow9099@163.com>
2022-05-21 14:34:59 +0800

    feat login

---
+++ src/components/LoginComp.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avator">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <div class="login_panel">
+        <el-form @keydown.enter.native="submitLogin('ruleForm')" :model="user" :rules="rules" ref="ruleForm" class="demo-form-inline">
+          <el-form-item label="账号" label-width="60px" prop="username">
+            <el-input
+            v-model="user.username" ref="nameRef"
+            prefix-icon="el-icon-user" placeholder="账号"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" label-width="60px" prop="password">
+            <el-input
+            v-model="user.password"
+             prefix-icon="el-icon-lock" type="password"  placeholder="密码"></el-input>
+          </el-form-item>
+          <el-form-item class="btns" >
+            <el-button type="primary" @click="submitLogin('ruleForm')">登录</el-button>
+            <el-button type="info" @click="reset('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginComp',
+  data () {
+    return {
+      user: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名账号', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  mounted () {
+    this.$refs.nameRef.focus()
+  },
+  methods: {
+    submitLogin (formName) {
+      this.$refs[formName].validate(async valid => {
+        if (valid) {
+          const { data: res } = await this.$http.post('login', this.user)
+          console.log(res)
+          if (res.meta.status !== 200) {
+            this.$message.error('登录失败')
+            return
+          }
+          this.$message.success('登录成功')
+          sessionStorage.setItem('token', res.data.token)
+          this.$router.replace('/home')
+        } else {
+          console.log('error')
+          return false
+        }
+      })
+    },
+    reset (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.login_container {
+  background: #3b4b6b;
+  height: 100%;
+  position: relative;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  border-radius: 4px;
+  background-color: #fff;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  .avator {
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 99;
+    background-color: #fff;
+    padding: 5px;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #52658b;
+    }
+  }
+}
+.login_panel{
+  margin-top: 70px;
+  padding: 0 10px;
+}
+.btns {
+  text-align: center;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import LoginComp from '../components/LoginComp.vue'
+import HomeComp from '../components/HomeComp.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: LoginComp },
+  { path: '/home', component: HomeComp }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const tokenStr = sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
